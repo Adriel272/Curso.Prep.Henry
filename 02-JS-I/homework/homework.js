a57d1fdaefd3c7033c79d37dea87ac25fb1404d5
Adriel Baez <estebanadriel2010@hotmail.com>
2021-01-01 16:35:22 -0300

    finish homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Soy Adriel';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 27;
+const nuevoNum = 271;
 
 // Crea una variable booleana:
 const nuevoBool = true;
